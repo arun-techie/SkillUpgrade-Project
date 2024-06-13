370da1815239574fc0a60f91f7fd4818ae7349dc
arun-techie <arunaadvik1@gmail.com>
2024-06-13 15:37:19 +0530

    Initial commit

---
+++ SU-TASK3/script.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = totalSlides - 1;
+    } else {
+        slideIndex = index;
+    }
+    const offset = -slideIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(slideIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(slideIndex - 1);
+}
+
+setInterval(() => {
+    nextSlide();
+}, 3000); // Change image every 3 seconds
+
+// Initial display
+showSlide(slideIndex);
